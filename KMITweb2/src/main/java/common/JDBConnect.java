732c5db10ec7a1eee0747a75243a249c4e9f3b8b
SH <acehyuk1988@naver.com>
2024-01-27 14:11:18 +0900

    BoardDAO, BoardDTO 작성

--- KMITweb2/src/main/java/common/JDBConnect.java
+++ KMITweb2/src/main/java/common/JDBConnect.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import javax.servlet.ServletContext;
+
 public class JDBConnect {
 	public Connection con;
 	public Statement stmt;
@@ -25,6 +27,23 @@ public class JDBConnect {
 			e.printStackTrace();
 		}
 	}
+	
+	public JDBConnect(ServletContext application) {
+		try {
+			String driver = application.getInitParameter("OracleDriver");
+			Class.forName(driver);
+						
+			String url = application.getInitParameter("OracleURL");
+			String id = application.getInitParameter("OracleId");
+			String pwd = application.getInitParameter("OraclePwd");
+			con = DriverManager.getConnection(url, id, pwd);
+
+			System.out.println("DB 연결 성공(인수 생성자 2)");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 
 	public void close() {
 		try {
